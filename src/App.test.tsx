3f635208dfabf5519c0d6d9b4ee03eea198a1c36
arianna choza <unachoza@gmail.com>
2025-02-21 23:52:02 -0500

    react, typescript and vite set up files configs

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("App component", () => {
+	it("should render text", () => {
+		render(<App />);
+		const h1Element = screen.getByRole("heading", { level: 1 });
+		expect(h1Element).toBeInTheDocument();
+		expect(h1Element).toHaveTextContent("Vite + React");
+	});
+});
