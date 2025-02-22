3be150f0479a0c1382025e464d6b8001f7016bbb
arianna choza <unachoza@gmail.com>
2025-02-22 13:27:52 -0500

    card images and form layout

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,8 +5,8 @@ import App from "./App";
 describe("App component", () => {
 	it("should render text", () => {
 		render(<App />);
-		const h1Element = screen.getByRole("heading", { level: 1 });
-		expect(h1Element).toBeInTheDocument();
-		expect(h1Element).toHaveTextContent("Vite + React");
+		// const h1Element = screen.getByRole("heading", { level: 1 });
+		// expect(h1Element).toBeInTheDocument();
+		// expect(h1Element).toHaveTextContent("Vite + React");
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FormEvent, useState } from "react";
-import SidePanelImg from "../src/assets/images/bg-main-desktop.png";
-import mobilePanelImg from "../src/assets/images/bg-main-mobile.png";
+import SidePanelImage from "../src/assets/images/bg-main-desktop.png";
+import mobilePanelImage from "../src/assets/images/bg-main-mobile.png";
+import cardFrontImage from "../src/assets/images/bg-card-front.png";
+import cardBackImage from "../src/assets/images/bg-card-back.png";
 import "./App.css";
 
 function App() {
 	const calcImageSize = () => {
-		return window.innerWidth < 600 ? mobilePanelImg : SidePanelImg;
+		return window.innerWidth < 600 ? mobilePanelImage : SidePanelImage;
 	};
 
 	const handleSubmit = (e: FormEvent) => {
@@ -14,31 +16,39 @@ function App() {
 	return (
 		<>
 			<img src={calcImageSize()} className="side-panel" alt="decoration" />
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="card-name">
-					cardholder name
-					<input id="card-name" type="text" placeholder="e.g. Jane Appleseed" />
-				</label>
-				<label htmlFor="card-number">
-					card number
-					<input id="card-name" type="number" placeholder="e.g. 1234 5678 9123 0000" />
-				</label>
-				<div className="date-cvc-section">
-					<div className="month-year-container">
-						<label htmlFor="exp-date">EXP Date MM/YY</label>
-						<div className="month-year-inputs">
-							<input type="number" name="" id="exp-date" className="month-year-input" placeholder="MM" />
-							<input type="number" name="" id="exp-date" className="month-year-input" placeholder="YY" />
-						</div>
-					</div>
-
-					<label htmlFor="cvc">
-						CVC
-						<input id="cvc" type="number" className="cvc-input" placeholder="e.g. 123" />
-					</label>
+			<div className="container">
+				<div className="card-responses-container">
+					<img id="cardFront" className="card" src={cardFrontImage} alt="card-front" />
+					<img id="cardBack" className="card" src={cardBackImage} alt="card-back" />
 				</div>
-			</form>
-			<div className="card"></div>
+				<div className="card-input-container">
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="card-name">
+							cardholder name
+							<input id="card-name" type="text" placeholder="e.g. Jane Appleseed" />
+						</label>
+						<label htmlFor="card-number">
+							card number
+							<input id="card-name" type="number" placeholder="e.g. 1234 5678 9123 0000" />
+						</label>
+						<div className="date-cvc-section">
+							<div className="month-year-container">
+								<label htmlFor="exp-date">EXP Date MM/YY</label>
+								<div className="month-year-inputs">
+									<input type="number" name="" id="exp-date" className="month-year-input" placeholder="MM" />
+									<input type="number" name="" id="exp-date" className="month-year-input" placeholder="YY" />
+								</div>
+							</div>
+
+							<label htmlFor="cvc">
+								CVC
+								<input id="cvc" type="number" className="cvc-input" placeholder="e.g. 123" />
+							</label>
+						</div>
+					</form>
+				</div>
+				<div className="card"></div>
+			</div>
 		</>
 	);
 }
