7b4226dc0a63b1acc0ebdbe7175cc7bd27050ecd
arianna choza <unachoza@gmail.com>
2025-02-22 00:59:19 -0500

    basic form outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from "react";
+import SidePanelImg from "../src/assets/images/bg-main-desktop.png";
+import mobilePanelImg from "../src/assets/images/bg-main-mobile.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const calcImageSize = () => {
+		return window.innerWidth < 600 ? mobilePanelImg : SidePanelImg;
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+	};
+	return (
+		<>
+			<img src={calcImageSize()} className="side-panel" alt="decoration" />
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="card-name">
+					cardholder name
+					<input id="card-name" type="text" placeholder="e.g. Jane Appleseed" />
+				</label>
+				<label htmlFor="card-number">
+					card number
+					<input id="card-name" type="number" placeholder="e.g. 1234 5678 9123 0000" />
+				</label>
+				<div className="date-cvc-section">
+					<div className="month-year-container">
+						<label htmlFor="exp-date">EXP Date MM/YY</label>
+						<div className="month-year-inputs">
+							<input type="number" name="" id="exp-date" className="month-year-input" placeholder="MM" />
+							<input type="number" name="" id="exp-date" className="month-year-input" placeholder="YY" />
+						</div>
+					</div>
+
+					<label htmlFor="cvc">
+						CVC
+						<input id="cvc" type="number" className="cvc-input" placeholder="e.g. 123" />
+					</label>
+				</div>
+			</form>
+			<div className="card"></div>
+		</>
+	);
 }
 
-export default App
+export default App;
+
+// 0000 0000 0000 0000
+// Jane Appleseed
+// 00/00
+
+// 000
+
+// Cardholder Name
+// e.g. Jane Appleseed
+
+// Card Number
+// e.g. 1234 5678 9123 0000
+
+// Exp. Date (MM/YY)
+// MM
+// YY
+
+// CVC
+// e.g. 123
+
+// Confirm
+
+// <!-- Completed state start -->
+
+// Thank you!
+// We've added your card details
+// Continue
